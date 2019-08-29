5a78c38dbe42759349a59fbe60fd331751c3f9ea
Małgorzata Jańczak <m.janczak@int.pl>
2019-08-29 17:05:18 +0200

    skrócenie kodu js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,24 @@
 'use strict';
 
 function titleClickHandler(event){
+
   event.preventDefault();
   const clickedElement = this;
 
   /* [DONE] remove class 'active' from all article links  */
-  const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLink = document.querySelector('.titles a.active');
+  activeLink.classList.remove('active');
 
-  for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
-  }
   /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts .post.active');
+  const activeArticle = document.querySelector('.posts .post.active');
+  activeArticle.classList.remove('active');
 
-  for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-  }
   /* [DONE] get 'href' attribute from the clicked link */
-  const hrefAttr = clickedElement.getAttribute('href').replace("#", "");
-
-  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
-
-  const articles = document.querySelectorAll('.posts .post');
-
-  for(let article of articles) {
-    if (article.getAttribute('id') == hrefAttr) {   /* add class 'active' to the correct article */
-      article.classList.add('active');
-    }
-  }
+  const hrefAttr = clickedElement.getAttribute('href');
+  document.querySelector(hrefAttr).classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
