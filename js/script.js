0d2b72a6ad676016586de69a9cd5ff5954b75b99
Małgorzata Jańczak <m.janczak@int.pl>
2019-08-29 12:23:09 +0200

    add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -2,17 +2,19 @@
 
 function titleClickHandler(event){
   console.log('Link was clicked!');
-  console.log(event);
+  const clickedElement = this;
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  console.log('Clicked Element:' , clickedElement);
+  clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .post.active');
 
   for(let activeArticle of activeArticles){
