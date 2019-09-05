3f8cdaffee464c21edd5f5f3e46a9d75b6e6e588
Małgorzata Jańczak <m.janczak@int.pl>
2019-09-06 00:10:33 +0200

    add tags array

--- js/script.js
+++ js/script.js
@@ -29,6 +29,7 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
+  optTagsListSelector = '.tags.list',
   optArticleAuthorSelector = '.post-author';
 
 function generateTitleLinks(customSelector = ''){
@@ -65,6 +66,8 @@ function generateTitleLinks(customSelector = ''){
 generateTitleLinks();
 
 function generateTags(){
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
   /* START LOOP: for every article: */
@@ -83,12 +86,21 @@ function generateTags(){
       const linkHtml = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>' + ' ';
       /* add generated code to html variable */
       html = html + linkHtml;
+      /* [NEW] check if this link is NOT already in allTags */
+      if(allTags.indexOf(linkHtml) == -1){
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHtml);
+      }
     /* END LOOP: for each tag */
     }
     /* insert HTML of all the links into the tags wrapper */
     tagsWrapper.innerHTML = html;
-  /* END LOOP: for every article: */
+    /* END LOOP: for every article: */
   }
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector('.tags');
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(' ');
 }
 
 generateTags();
